6ad859d17bd201c26ed33aebb88998f82d0efc4b
Pavlo Zimnytsia <paha20zimnica16@gmail.com>
2023-04-30 05:39:49 +0300

    Updated files

--- src/PassGen.java
+++ src/PassGen.java
@@ -4,64 +4,61 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.security.SecureRandom;
 
+public class PassGen extends Frame implements ActionListener, GUI {
+    GUIElements guiElements = new GUIElements();
 
+    public static void main(String[] args) {
+        new PassGen();
+    }
 
-public class PassGen extends Frame implements ActionListener {
-
-    JTextField generatedPass;
-    JTextField length;
-    JLabel label;
-    JLabel stripe;
-    JLabel label1;
-    JLabel symbols;
-    JButton button;
-    JButton exit;
-    JCheckBox checkBox;
+    public void exit(ActionEvent e){
+        System.exit(0);
+    }
 
     PassGen(){
 
-        length = new JTextField();
-        length.setBounds(220, 58, 35, 25);
+        guiElements.length = new JTextField();
+        guiElements.length.setBounds(220, 58, 35, 25);
 
-        generatedPass = new JTextField();
-        generatedPass.setBounds(25, 200, 300, 25);
+        guiElements.generatedPass = new JTextField();
+        guiElements.generatedPass.setBounds(25, 200, 300, 25);
         //---------------------------------------------------------
-        button = new JButton("Generate password");
-        button.setBounds(95, 240, 150, 35);
-        button.addActionListener(this);
+        guiElements.button = new JButton("Generate password");
+        guiElements.button.setBounds(95, 240, 150, 35);
+        guiElements.button.addActionListener(this);
 
-        exit = new JButton("EXIT");
-        exit.setBounds(120, 340, 100, 35);
-        exit.addActionListener(this:: exit);
+        guiElements.exit = new JButton("EXIT");
+        guiElements.exit.setBounds(120, 340, 100, 35);
+        guiElements.exit.addActionListener(this:: exit);
         //---------------------------------------------------------
-        label = new JLabel("Your password is:");
-        label.setBounds(30, 160, 200, 50);
+        guiElements.label = new JLabel("Your password is:");
+        guiElements.label.setBounds(30, 160, 200, 50);
 
-        label1 = new JLabel("The length of the password:");
-        label1.setBounds(55, 45, 200, 50);
+        guiElements.label1 = new JLabel("The length of the password:");
+        guiElements.label1.setBounds(55, 45, 200, 50);
 
-        symbols = new JLabel("Use special symbols: ");
-        symbols.setBounds(55, 75, 200, 50);
+        guiElements.symbols = new JLabel("Use special symbols: ");
+        guiElements.symbols.setBounds(55, 75, 200, 50);
 
-        stripe = new JLabel("_________________________________________");
-        stripe.setBounds(30, 150, 300, 20);
+        guiElements.stripe = new JLabel("_________________________________________");
+        guiElements.stripe.setBounds(30, 150, 300, 20);
         //---------------------------------------------------------
-        checkBox = new JCheckBox();
-        checkBox.setBounds(218, 90, 20, 20);
+        guiElements.checkBox = new JCheckBox();
+        guiElements.checkBox.setBounds(218, 90, 20, 20);
 
 
-        add(length);
-        add(generatedPass);
-
-        add(button);
-        add(exit);
-
-        add(label);
-        add(label1);
-        add(stripe);
-        add(symbols);
-
-        add(checkBox);
+        add(guiElements.length);
+        add(guiElements.generatedPass);
+        //---------------------------------------------------------
+        add(guiElements.button);
+        add(guiElements.exit);
+        //---------------------------------------------------------
+        add(guiElements.label);
+        add(guiElements.label1);
+        add(guiElements.stripe);
+        add(guiElements.symbols);
+        //---------------------------------------------------------
+        add(guiElements.checkBox);
         //---------------------------------------------------------
         setLayout(null);
         setSize(350, 400);
@@ -71,35 +68,26 @@ public class PassGen extends Frame implements ActionListener {
     public void actionPerformed(ActionEvent e){
         String password;
 
-        if(checkBox.isSelected()){
-            password = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_@!#$%^&*()-+=/{}:|;?><~";
+        if(guiElements.checkBox.isSelected()){
+            password = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_@!#$%^&*()-+=/{}:|;?><~.,";
         }else {
             password = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         }
             SecureRandom random = new SecureRandom();
             StringBuilder stringBuilder = new StringBuilder();
             try {
-                int passLen = Integer.parseInt(length.getText());
-
+                int passLen = Integer.parseInt(guiElements.length.getText());
                 if (passLen > 38) {
-                    generatedPass.setText("Too large pass!");
+                    guiElements.generatedPass.setText("Too large pass!");
                 } else {
                     for (int i = 0; i < passLen; i++) {
                         int randomIndex = random.nextInt(password.length());
                         stringBuilder.append(password.charAt(randomIndex));
                     }
-                    generatedPass.setText(stringBuilder.toString());
+                    guiElements.generatedPass.setText(stringBuilder.toString());
                 }
             } catch (Exception exception) {
-                generatedPass.setText("Incompatible data types!");
+                guiElements.generatedPass.setText("Incompatible data types!");
             }
-        }
-
-    public void exit(ActionEvent e){
-        System.exit(0);
-    }
-
-    public static void main(String[] args) {
-        new PassGen();
     }
 }
